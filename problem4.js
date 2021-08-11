d5970d21df1b719ac6a56ef3508ca039262159a5
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-08-11 09:52:30 +0530

    Solution of Problem4

---
+++ problem4.js
@@ -0,0 +1,9 @@
+function problem4 (inventory) {  //return the list of car years from inventory
+    let list = []
+    for (let i =0; i<inventory.length; i++){
+        let obj = inventory[i]
+        list.push(obj['car_year'])
+    }
+    return list
+}
+module.exports.problem4 = problem4;
